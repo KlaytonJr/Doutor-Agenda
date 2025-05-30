797023351a0a70e67cbe629062ae2a76e271e632
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-05-30 16:04:13 -0300

    feat: add stripe webhook processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { redirect } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
+  redirect("/authentication");
   return (
     <div>
       <Button>Bootcamp</Button>
