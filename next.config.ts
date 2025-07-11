9a785ed235636b9158f468003e2d97d733232351
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-07-11 15:29:01 -0300

    feat: delete middleware

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
